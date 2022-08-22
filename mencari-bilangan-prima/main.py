75e9eefaaf1a59ea3752fa8ac2cd68acb728e6f0
aldojr <reynaldosgn@gmail.com>
2022-08-22 09:55:43 +0700

    Bilangan Prima Input Python

---
+++ mencari-bilangan-prima/main.py
@@ -0,0 +1,8 @@
+from prima import cari_bilangan_prima, is_prima
+
+awal = int (input ("Masukan nilai awal : "))
+akhir = int (input ("Masukan nilai akhir : "))
+
+
+
+print (cari_bilangan_prima(awal, akhir))
